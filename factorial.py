cb8671d09c0ff4d640306dfa991a06ed32f1c60c
hojoon <hj2758@gmail.com>
2021-06-25 11:30:46 +0900

    팩토리얼

--- factorial.py
+++ factorial.py
@@ -1,3 +1,8 @@
+# 팩토리얼 구하기
+
+# loop
+import math
+
 
 def factorial(n):
     a=1
@@ -5,4 +10,19 @@ def factorial(n):
         a *= i
     return a
 
-print(factorial(5))
+
+# recursive call
+a = 1
+def fac(n):
+    a = n * (n - 1)
+    if(n==1):
+        return a
+    else:
+        fac(n - 1)
+        print(a)
+
+
+print('반복문 : '+ str(factorial(5)))
+print('재귀호출 : '+ str(fac(5)))
+print('내장함수 : '+ str(math.factorial(5)))
+
